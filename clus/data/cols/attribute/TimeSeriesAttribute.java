09c6bd68b5290d350f758fb7e2bf53e05bfdbd6b
dragik <dragik@1a29536d-788d-45e4-adf6-63c72010a73e>
2006-04-05 23:03:15 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/clus/code/trunk@99 1a29536d-788d-45e4-adf6-63c72010a73e

---
+++ clus/data/cols/attribute/TimeSeriesAttribute.java
@@ -0,0 +1,23 @@
+package clus.data.cols.attribute;
+
+import java.io.IOException;
+
+import clus.data.type.TimeSeriesAttrType;
+import clus.io.ClusReader;
+
+public class TimeSeriesAttribute extends TimeSeriesAttrBase{
+
+	public double[][] m_Data;
+	
+	public TimeSeriesAttribute(TimeSeriesAttrType type) {
+		super(type);
+	}
+
+	@Override
+	public void read(ClusReader data, int row) throws IOException {
+		System.out.println("Samo proba");
+	}
+
+	
+	
+}
---
+++ clus/data/cols/attribute/TimeSeriesTarget.java
@@ -0,0 +1,26 @@
+package clus.data.cols.attribute;
+
+import java.io.IOException;
+
+import clus.data.cols.ColTarget;
+import clus.data.type.TimeSeriesAttrType;
+import clus.io.ClusReader;
+import clus.ext.timeseries.*;
+ 
+public class TimeSeriesTarget extends TimeSeriesAttrBase {
+
+	protected ColTarget m_Target;
+	protected int m_Index;	
+	
+	public TimeSeriesTarget(ColTarget target, TimeSeriesAttrType type, int index) {
+		super(type);
+		m_Target = target;
+		m_Index = index;
+	}
+	
+	@Override
+	public void read(ClusReader data, int row) throws IOException {
+		m_Target.setTimeSeries(m_Index, row, new TimeSeries(data.readTimeSeries()));
+	}
+
+}
