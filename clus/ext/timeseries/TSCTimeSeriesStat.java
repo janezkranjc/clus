28a94e0bc51ffa99889997a8a48c03bf4189f3d8
dragik <dragik@1a29536d-788d-45e4-adf6-63c72010a73e>
2006-04-18 12:48:54 +0000

    TimeSeriesCorrelation - TSC
    git-svn-id: svn://svn.code.sf.net/p/clus/code/trunk@112 1a29536d-788d-45e4-adf6-63c72010a73e

---
+++ clus/ext/timeseries/TSCTimeSeriesStat.java
@@ -0,0 +1,69 @@
+package clus.ext.timeseries;
+
+import clus.data.attweights.ClusAttributeWeights;
+import clus.data.rows.RowData;
+import clus.main.Settings;
+import clus.statistic.ClusStatistic;
+
+public class TSCTimeSeriesStat extends TimeSeriesStat{
+
+	public final static long serialVersionUID = Settings.SERIAL_VERSION_ID;
+
+	public double calcDistance(TimeSeries t1, TimeSeries t2) {
+		//this calculates the Correlation coefficient of two TimeSeries
+		//the two TimeSeries have same length
+		double[] ts1=t1.getValues();
+		double[] ts2=t2.getValues();
+		double mean_ts1=calculateMean(ts1);
+		double mean_ts2=calculateMean(ts2);
+		double cc=0;
+		double sum_ts1_sqr=0;
+		double sum_ts2_sqr=0;
+		double sum_ts1_ts2=0;
+		if (ts1.length!=ts2.length){
+			System.err.println("TimeSeriesCorrelation applies only to Time Series with equal length");
+			throw new ArrayIndexOutOfBoundsException("TimeSeriesCorrelation applies only to Time Series with equal length");
+		}
+		for (int k=0;k<ts1.length;k++){
+			sum_ts1_ts2+=(ts1[k]-mean_ts1)*(ts2[k]-mean_ts2);
+			sum_ts1_sqr+=Math.pow((ts1[k]-mean_ts1),2);
+			sum_ts2_sqr+=Math.pow((ts2[k]-mean_ts2),2);
+		}
+		cc=1-Math.abs(sum_ts1_ts2/Math.sqrt(sum_ts1_sqr*sum_ts2_sqr));
+		return cc;
+	}
+
+	public double getSS(ClusAttributeWeights scale, RowData data) {
+		// TODO Auto-generated method stub
+		return super.getSS(scale, data);
+	}
+
+	public void optimizePreCalc(RowData data) {
+		// TODO Auto-generated method stub
+		super.optimizePreCalc(data);
+	}
+
+	public ClusStatistic cloneStat() {
+		TSCTimeSeriesStat stat = new TSCTimeSeriesStat();
+		stat.cloneFrom(this);
+		return stat;
+	}
+
+	public void copy(ClusStatistic other) {
+		TSCTimeSeriesStat or = (TSCTimeSeriesStat) other;
+		super.copy(or);
+		m_Value = or.m_Value;
+	}
+
+	public double getError(ClusAttributeWeights scale) {
+		// TODO Auto-generated method stub
+		return Double.POSITIVE_INFINITY;
+	}
+	
+	public double calculateMean(double[] ts){
+		double sum =0;
+		for (int k =0;k<ts.length;k++)sum+=ts[k];
+		sum =sum / ts.length;
+		return sum;
+	}
+}
