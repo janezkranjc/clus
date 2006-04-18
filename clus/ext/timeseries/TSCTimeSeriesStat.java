1403f17a59c364e6378400b4619b67ecec7c4b53
dragik <dragik@1a29536d-788d-45e4-adf6-63c72010a73e>
2006-04-18 21:42:53 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/clus/code/trunk@115 1a29536d-788d-45e4-adf6-63c72010a73e

--- clus/ext/timeseries/TSCTimeSeriesStat.java
+++ clus/ext/timeseries/TSCTimeSeriesStat.java
@@ -32,7 +32,32 @@ public class TSCTimeSeriesStat extends TimeSeriesStat{
 		cc=1-Math.abs(sum_ts1_ts2/Math.sqrt(sum_ts1_sqr*sum_ts2_sqr));
 		return cc;
 	}
-
+	/*
+	public double calcDistance(TimeSeries t1, TimeSeries t2) {
+		//this calculates the Correlation coefficient of two TimeSeries
+		//the two TimeSeries may have different lengths - Cycle Arrays
+		
+		double[] ts1=t1.getValues();
+		double[] ts2=t2.getValues();
+		double mean_ts1=calculateMean(ts1);
+		double mean_ts2=calculateMean(ts2);
+		double cc=0;
+		double sum_ts1_sqr=0;
+		double sum_ts2_sqr=0;
+		double sum_ts1_ts2=0;
+		boolean run=true;
+		int k=0;
+		while (run){
+			sum_ts1_ts2+=(ts1[k%ts1.length]-mean_ts1)*(ts2[k%ts2.length]-mean_ts2);
+			sum_ts1_sqr+=Math.pow((ts1[k%ts1.length]-mean_ts1),2);
+			sum_ts2_sqr+=Math.pow((ts2[k%ts2.length]-mean_ts2),2);
+			k++;
+			if ((k%ts1.length==0)&&(k%ts2.length==0))run=false;
+		}
+		cc=1-Math.abs(sum_ts1_ts2/Math.sqrt(sum_ts1_sqr*sum_ts2_sqr));
+		return cc;
+	}*/
+	
 	public double getSS(ClusAttributeWeights scale, RowData data) {
 		// TODO Auto-generated method stub
 		return super.getSS(scale, data);
