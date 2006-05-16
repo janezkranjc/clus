9b8dba7b714ef0f94780bd5bfac8063763c41f4f
dragik <dragik@1a29536d-788d-45e4-adf6-63c72010a73e>
2006-05-16 23:13:12 +0000

    TSC - small changes
    git-svn-id: svn://svn.code.sf.net/p/clus/code/trunk@133 1a29536d-788d-45e4-adf6-63c72010a73e

--- clus/ext/timeseries/TSCTimeSeriesStat.java
+++ clus/ext/timeseries/TSCTimeSeriesStat.java
@@ -12,24 +12,25 @@ public class TSCTimeSeriesStat extends TimeSeriesStat{
 	public double calcDistance(TimeSeries t1, TimeSeries t2) {
 		//this calculates the Correlation coefficient of two TimeSeries
 		//the two TimeSeries have same length
-		double[] ts1=t1.getValues();
-		double[] ts2=t2.getValues();
-		double mean_ts1=calculateMean(ts1);
-		double mean_ts2=calculateMean(ts2);
-		double cc=0;
-		double sum_ts1_sqr=0;
-		double sum_ts2_sqr=0;
-		double sum_ts1_ts2=0;
-		if (ts1.length!=ts2.length){
+		double[] ts1 = t1.getValues();
+		double[] ts2 = t2.getValues();
+		double mean_ts1 = calculateMean(ts1);
+		double mean_ts2 = calculateMean(ts2);
+		double cc = 0;
+		double sum_ts1_sqr = 0;
+		double sum_ts2_sqr = 0;
+		double sum_ts1_ts2 = 0;
+		if (ts1.length != ts2.length){
 			System.err.println("TimeSeriesCorrelation applies only to Time Series with equal length");
-			throw new ArrayIndexOutOfBoundsException("TimeSeriesCorrelation applies only to Time Series with equal length");
+	//		throw new ArrayIndexOutOfBoundsException("TimeSeriesCorrelation applies only to Time Series with equal length");
+			System.exit(1);
 		}
-		for (int k=0;k<ts1.length;k++){
-			sum_ts1_ts2+=(ts1[k]-mean_ts1)*(ts2[k]-mean_ts2);
-			sum_ts1_sqr+=Math.pow((ts1[k]-mean_ts1),2);
-			sum_ts2_sqr+=Math.pow((ts2[k]-mean_ts2),2);
+		for (int k = 0; k < ts1.length; k++){
+			sum_ts1_ts2 += (ts1[k] - mean_ts1) * (ts2[k] - mean_ts2);
+			sum_ts1_sqr += Math.pow((ts1[k] - mean_ts1),2);
+			sum_ts2_sqr += Math.pow((ts2[k] - mean_ts2),2);
 		}
-		cc=1-Math.abs(sum_ts1_ts2/Math.sqrt(sum_ts1_sqr*sum_ts2_sqr));
+		cc = 1 - Math.abs(sum_ts1_ts2/Math.sqrt(sum_ts1_sqr*sum_ts2_sqr));
 		return cc;
 	}
 	/*
@@ -86,9 +87,9 @@ public class TSCTimeSeriesStat extends TimeSeriesStat{
 	}
 	
 	public double calculateMean(double[] ts){
-		double sum =0;
-		for (int k =0;k<ts.length;k++)sum+=ts[k];
-		sum =sum / ts.length;
+		double sum = 0;
+		for (int k = 0; k < ts.length; k++)sum += ts[k];
+		sum = sum / ts.length;
 		return sum;
 	}
 }
