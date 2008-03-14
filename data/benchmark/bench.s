5d4f5b7e1fa199ce6a67af384a27cd95f413c202
beau <beau@1a29536d-788d-45e4-adf6-63c72010a73e>
2008-03-14 09:57:43 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/clus/code/trunk@288 1a29536d-788d-45e4-adf6-63c72010a73e

---
+++ data/benchmark/bench.s
@@ -0,0 +1,25 @@
+[Attributes]
+Disable = 48-83
+Target = 48-83
+Descriptive = 1-47
+Weights = 1
+
+[SIT]
+Main_target = 49
+Recursive = yes
+Variant = SS
+
+[Data]
+File = bench.arff
+
+XVal = 26
+
+[Output] 
+AllFoldModels = No
+AllFoldErrors = Yes
+WritePredictions = {Test}
+
+[General]
+Verbose = 0
+RandomSeed = 0
+
