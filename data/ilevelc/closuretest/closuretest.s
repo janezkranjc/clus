3a0c94e5dbad1402074a6a6e6294c0bcd38b38f5
jan <jan@1a29536d-788d-45e4-adf6-63c72010a73e>
2008-07-23 05:57:07 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/clus/code/trunk@341 1a29536d-788d-45e4-adf6-63c72010a73e

---
+++ data/ilevelc/closuretest/closuretest.s
@@ -0,0 +1,13 @@
+
+[Attributes]
+Descriptive = 1-2
+Clustering = 1-2
+Target = 1-2
+Weights = 1.0
+
+[ILevelC]
+File = closuretest.ilevelc
+RunCOPKMeans = Yes
+
+[Model]
+MinimalWeight = 1.0
---
+++ data/ilevelc/iris/iris-copkmeans.s
@@ -0,0 +1,12 @@
+[Data]
+File = ../../iris/iris.arff
+
+[Attributes]
+Descriptive = 1-4
+Clustering = 1-4
+Target = 1-4
+Weights = 1.0
+
+[ILevelC]
+File = iris.constraints
+RunCOPKMeans = Yes
