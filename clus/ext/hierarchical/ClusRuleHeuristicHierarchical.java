aa6f60da4c05c46b2a5a4b3bc65eedfb30daf388
ben <ben@1a29536d-788d-45e4-adf6-63c72010a73e>
2008-11-12 12:53:26 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/clus/code/trunk@396 1a29536d-788d-45e4-adf6-63c72010a73e

---
+++ clus/ext/hierarchical/ClusRuleHeuristicHierarchical.java
@@ -0,0 +1,55 @@
+package clus.ext.hierarchical;
+
+import clus.data.attweights.ClusAttributeWeights;
+import clus.data.type.ClusAttrType;
+import clus.heuristic.ClusHeuristic;
+import clus.main.ClusStatManager;
+import clus.main.Settings;
+import clus.statistic.ClusStatistic;
+
+public class ClusRuleHeuristicHierarchical extends ClusHeuristic {
+
+	protected ClusAttributeWeights m_TargetWeights;
+	protected ClusStatManager m_StatManager;
+	
+	public ClusRuleHeuristicHierarchical(ClusStatManager stat_mgr, ClusAttributeWeights prod) {
+		m_StatManager = stat_mgr;
+		m_TargetWeights = prod;
+	}
+	
+	/**
+	 * This heuristic calculates:
+	 * ( |S|.Var(S) - |Sr|.Var(Sr) ) . Coverage(r)
+	 */
+	public double calcHeuristic(ClusStatistic c_tstat, ClusStatistic c_pstat,
+			ClusStatistic missing) {
+		
+		double n_pos = c_pstat.m_SumWeight;
+		if (n_pos-Settings.MINIMAL_WEIGHT < 1e-6) { // (n_pos < Settings.MINIMAL_WEIGHT) 
+			return Double.NEGATIVE_INFINITY;
+		}
+		
+		// Calculate |S|.Var(S) - |Sr|.Var(Sr)
+		//WHTDStatistic tstat = (WHTDStatistic) m_StatManager.getTrainSetStat(); // Geeft classcastexception (is blijkbaar een CombStat)
+		WHTDStatistic tstat = (WHTDStatistic) m_StatManager.getStatistic(ClusAttrType.ATTR_USE_TARGET); // (is altijd 0...)
+		double totalValue = tstat.getSS(m_TargetWeights);
+		double ruleValue = c_pstat.getSS(m_TargetWeights);
+		double value = totalValue - ruleValue;
+		
+		
+		//System.out.println("Difference made by rule: " + totalValue + " - " + ruleValue);
+		
+		// Coverage(r) part
+	  	double train_sum_w = m_StatManager.getTrainSetStat().getTotalWeight();
+	    double coverage = (n_pos/train_sum_w);
+	    value = value * coverage;
+		
+	    //System.out.println("Totale Heuristiek: " + value + " Coverage: " + coverage);
+		return value;
+	}
+
+	public String getName() {
+		return "RuleHeuristicHierarchical";
+	}
+
+}
