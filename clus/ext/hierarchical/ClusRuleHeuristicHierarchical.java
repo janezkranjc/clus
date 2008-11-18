30a6b59b46e13b95d35352e7ee3e156193fc1464
ben <ben@1a29536d-788d-45e4-adf6-63c72010a73e>
2008-11-18 15:29:03 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/clus/code/trunk@408 1a29536d-788d-45e4-adf6-63c72010a73e

--- clus/ext/hierarchical/ClusRuleHeuristicHierarchical.java
+++ clus/ext/hierarchical/ClusRuleHeuristicHierarchical.java
@@ -31,7 +31,8 @@ public class ClusRuleHeuristicHierarchical extends ClusHeuristic {
 		
 		// Calculate |S|.Var(S) - |Sr|.Var(Sr)
 		//WHTDStatistic tstat = (WHTDStatistic) m_StatManager.getTrainSetStat(); // Geeft classcastexception (is blijkbaar een CombStat)
-		WHTDStatistic tstat = (WHTDStatistic) m_StatManager.getStatistic(ClusAttrType.ATTR_USE_TARGET); // (is altijd 0...)
+		//WHTDStatistic tstat = (WHTDStatistic) m_StatManager.getStatistic(ClusAttrType.ATTR_USE_TARGET); // (is altijd 0...)
+		WHTDStatistic tstat = (WHTDStatistic) m_StatManager.getTrainSetStat(ClusAttrType.ATTR_USE_CLUSTERING);
 		double totalValue = tstat.getSS(m_TargetWeights);
 		double ruleValue = c_pstat.getSS(m_TargetWeights);
 		double value = totalValue - ruleValue;
