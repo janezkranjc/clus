6b959cae168fc49d28a5dc412bbeeed84361528a
tinodj <tinodj@1a29536d-788d-45e4-adf6-63c72010a73e>
2009-02-25 15:37:18 +0000

    Added coment for implementation of dtw
    git-svn-id: svn://svn.code.sf.net/p/clus/code/trunk@509 1a29536d-788d-45e4-adf6-63c72010a73e

--- clus/ext/timeseries/DTWTimeSeriesStat.java
+++ clus/ext/timeseries/DTWTimeSeriesStat.java
@@ -39,6 +39,13 @@ public class DTWTimeSeriesStat extends TimeSeriesStat {
 		super(attr);
 	}
 	
+	/*
+	 * For implementation details please see
+	 * Yuu Yamada et.al. 
+	 * Decision-tree Induction from Time-series Data Based on a Standard-example Split Test
+	 * http://www.hpl.hp.com/conferences/icml2003/papers/145.pdf
+	 * 
+	 */
 	public double calcDistance(TimeSeries t1, TimeSeries t2, int adjustmentWindow){
 
 		int m = t1.length();
