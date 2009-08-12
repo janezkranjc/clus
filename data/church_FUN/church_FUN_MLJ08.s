098c19aa5848b12f60c403d7950996a68ae5dc31
leander <leander@1a29536d-788d-45e4-adf6-63c72010a73e>
2009-08-12 13:07:04 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/clus/code/trunk@591 1a29536d-788d-45e4-adf6-63c72010a73e

--- data/church_FUN/church_FUN_MLJ08.s
+++ data/church_FUN/church_FUN_MLJ08.s
@@ -23,7 +23,7 @@ ClassificationThreshold = [0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,3
 
 [Tree]
 ConvertToRules = No
-FTest = 0.005 % [0.001,0.005,0.01,0.05,0.1,0.125]
+FTest = [0.001,0.005,0.01,0.05,0.1,0.125]
 
 [Model]
 MinimalWeight = 5.0
