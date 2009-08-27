3375215da51c0f94f67d1dd8b1fe6d2d24dcefd7
celine <celine@1a29536d-788d-45e4-adf6-63c72010a73e>
2009-08-27 13:52:53 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/clus/code/trunk@604 1a29536d-788d-45e4-adf6-63c72010a73e

--- clus/algo/tdidt/DepthFirstInduceSparse.java
+++ clus/algo/tdidt/DepthFirstInduceSparse.java
@@ -49,7 +49,13 @@ public class DepthFirstInduceSparse extends DepthFirstInduce {
 		}		
 	}
 	
+	
 	public void induce(ClusNode node, RowData data) {
+		if (getSettings().isEnsembleMode() && (getSettings().getEnsembleMethod() == Settings.ENSEMBLE_RFOREST))
+		{
+			induceRandomForest(node,data);
+		}
+		else {
 		ClusAttrType[] attrs = getDescriptiveAttributes();
 		initializeExamples(attrs, data);
 		ArrayList<ClusAttrType> attrList = new ArrayList<ClusAttrType>();
@@ -79,6 +85,7 @@ public class DepthFirstInduceSparse extends DepthFirstInduce {
 		Object[] attrArray = attrList.toArray();	
 		Object[] examplelistArray = examplelistList.toArray();	
 		induce(node, data, attrArray, examplelistArray);
+		}
 	}	
 	
 	
@@ -165,4 +172,87 @@ public class DepthFirstInduceSparse extends DepthFirstInduce {
 		}
 		return weight;
 	}
+	
+	
+	
+	
+	
+	// for random forests, a different induce approach is taken, because at each node, we have a different set of attributes
+	public void induceRandomForest(ClusNode node, RowData data) {
+		ClusAttrType[] attrs = getSchema().getDescriptiveAttributes();
+		initializeExamples(attrs, data);
+		induceRandomForestRecursive(node,data);
+	}
+	
+	public void induceRandomForestRecursive(ClusNode node, RowData data) {
+		ClusAttrType[] attrs = getDescriptiveAttributes();
+		ArrayList<ClusAttrType> attrList = new ArrayList<ClusAttrType>();
+		for (int i = 0; i < attrs.length; i++) {
+			ClusAttrType at = attrs[i];
+			if (at.isSparse()) {
+				if (((SparseNumericAttrType)at).getExampleWeight() >= getSettings().getMinimalWeight()) {
+					attrList.add(at);
+				}
+			}
+			else {
+				attrList.add(at);
+			}
+		}
+		Object[] attrArray = attrList.toArray();	
+		induceRandomForestRecursive2(node, data, attrArray);
+
+	}
+	
+	public void induceRandomForestRecursive2(ClusNode node, RowData data, Object[] attrs) {
+		//System.out.println("INDUCE SPARSE with " + attrs.length + " attributes and " + data.getNbRows() + " examples");
+		// Initialize selector and perform various stopping criteria
+		if (initSelectorAndStopCrit(node, data)) {
+			makeLeaf(node);
+			return;
+		}
+		// Find best test
+		for (int i = 0; i < attrs.length; i++) {
+			ClusAttrType at = (ClusAttrType)attrs[i];
+			if (at instanceof NominalAttrType) m_FindBestTest.findNominal((NominalAttrType)at, data);
+			else m_FindBestTest.findNumeric((NumericAttrType)at, data);
+		}
+		
+		// Partition data + recursive calls
+		CurrentBestTestAndHeuristic best = m_FindBestTest.getBestTest();
+		if (best.hasBestTest()) {
+			node.testToNode(best);
+			// Output best test
+			if (Settings.VERBOSE > 0) System.out.println("Test: "+node.getTestString()+" -> "+best.getHeuristicValue());
+			// Create children
+			int arity = node.updateArity();
+			NodeTest test = node.getTest();
+			RowData[] subsets = new RowData[arity];
+			for (int j = 0; j < arity; j++) {
+				subsets[j] = data.applyWeighted(test, j);
+			}
+			if (getSettings().showAlternativeSplits()) {
+				filterAlternativeSplits(node, data, subsets);
+			}
+			if (node != m_Root && getSettings().hasTreeOptimize(Settings.TREE_OPTIMIZE_NO_INODE_STATS)) {
+				// Don't remove statistics of root node; code below depends on them
+				node.setClusteringStat(null);
+				node.setTargetStat(null);
+			}
+
+			for (int j = 0; j < arity; j++) {
+				ClusNode child = new ClusNode();
+				node.setChild(child, j);
+				child.initClusteringStat(m_StatManager, m_Root.getClusteringStat(), subsets[j]);
+				child.initTargetStat(m_StatManager, m_Root.getTargetStat(), subsets[j]);
+
+				induceRandomForestRecursive(child, subsets[j]);
+			}
+		} else {
+			makeLeaf(node);
+		}
+	}
+	
+	
+	
+	
 }
